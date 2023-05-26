0f2c91a66c750fee3e3a7a1cc8dd7b01ad780eac
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-05-25 22:54:48 -0300

    feat: add queue service adapter

---
+++ src/layers/external/queue/adapter/index.ts
@@ -0,0 +1,26 @@
+import { Message } from "amqplib";
+import { QueueServiceProtocol } from "@/layers/send-email-use-case";
+import { QueueHelper } from "../helper";
+
+export class QueueServiceAdapter implements QueueServiceProtocol {
+
+    private deserializeMessage<Type>(message: Message): Type {
+        return JSON.parse(message.content.toString()) as Type;
+    }
+
+    async sendMessage(queue: string, object: object): Promise<void> {
+        QueueHelper.channel.sendToQueue(queue, Buffer.from(JSON.stringify(object)));
+    }
+	
+    async consumeMessage<Type>(queue: string, callback: (message: Type) => Promise<void>): Promise<void> {
+        QueueHelper.channel.consume(queue, async (message) => {
+            try {
+                const deserializedMessage = this.deserializeMessage<Type>(message);
+                await callback(deserializedMessage);
+                QueueHelper.channel.ack(message);
+            } catch(e) {
+                console.log(e);
+            }
+        });
+    }
+}
---
+++ src/layers/external/queue/helper/index.ts
@@ -0,0 +1,23 @@
+import { QUEUE_HOST } from "@/shared";
+import { Channel, Connection, connect } from "amqplib";
+
+export class QueueHelper {
+
+    private static connection: Connection | null = null;
+    static channel: Channel;
+
+    static async connect(): Promise<void> {
+        if(!QueueHelper.connection) {
+            QueueHelper.connection = await connect(QUEUE_HOST);
+            QueueHelper.channel = await QueueHelper.connection.createChannel();
+        }
+    }
+
+    static async disconnect(): Promise<void> {
+        await QueueHelper.connection.close();
+    }
+
+    static async cancel(queue: string): Promise<void> {
+        await QueueHelper.channel.cancel(queue);
+    }
+}
