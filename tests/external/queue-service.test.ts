d6b75d9efa99c1d05b765fd9ab8e236ec3f3039d
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-05-25 23:01:12 -0300

    test: add queue service adapter integration test

---
+++ tests/external/queue-service.test.ts
@@ -0,0 +1,33 @@
+import { QueueServiceAdapter, QueueHelper } from "@/layers/external";
+
+describe("External - QueueServiceAdapter", () => {
+    
+    const queue = "queue";
+
+    beforeAll(async () => await QueueHelper.connect());
+    afterAll(async () => {
+        await QueueHelper.cancel(queue);
+        await QueueHelper.disconnect();
+    });
+
+    test("Should send message | sendMessage", async () => {
+        const sut = new QueueServiceAdapter();
+        jest.spyOn(sut, "sendMessage");
+        
+        await sut.sendMessage("queue", { name: "João" });
+
+        expect(sut.sendMessage).toHaveBeenCalled();
+        expect(sut.sendMessage).toHaveBeenCalledWith("queue", { name: "João" });
+    });
+
+    test("Should receive message | sendMessage", async () => {
+        const sut = new QueueServiceAdapter();
+        jest.spyOn(sut, "sendMessage");
+        await sut.sendMessage(queue, { name: "João" });
+		
+        await sut.consumeMessage<{ name: string }>("queue", async (message) => expect(message.name).toBe("João"));
+
+        expect(sut.sendMessage).toHaveBeenCalled();
+        expect(sut.sendMessage).toHaveBeenCalledWith("queue", { name: "João" });
+    });
+});
