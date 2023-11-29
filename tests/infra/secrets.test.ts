53050698b5080e4e431702f75de8ad2d05f2c90e
João Pedro <112661258+JPedro109@users.noreply.github.com>
2023-11-29 19:37:19 -0300

    test: add secrets service integration tests

---
+++ tests/infra/secrets.test.ts
@@ -0,0 +1,62 @@
+import { SecretsServiceAdapter, SecretsEnum } from "@/infra";
+
+describe("Infra - SecretsServiceAdapter", () => {
+
+    test("Should get secret | getSecret", async () => {
+        const variable = SecretsEnum.Secret;
+        process.env[variable] = "Secret";
+        const sut = new SecretsServiceAdapter();
+        jest.spyOn(sut, "getSecret");
+
+        const result = sut.getSecret(variable);
+
+        expect(result).toBe("Secret");
+        expect(sut.getSecret).toHaveBeenCalled();
+        expect(sut.getSecret).toHaveBeenCalledWith(variable);
+    });
+
+    test("Should get secret null | getRequiredSecret", async () => {
+        const variable = SecretsEnum.Secret;
+        delete process.env[variable];
+        const sut = new SecretsServiceAdapter();
+        jest.spyOn(sut, "getSecret");
+
+        const result = sut.getSecret(variable);
+
+        expect(result).toBeNull();
+        expect(sut.getSecret).toHaveBeenCalled();
+        expect(sut.getSecret).toHaveBeenCalledWith(variable);
+    });
+
+    test("Should get error | getRequiredSecret", async () => {
+        const variable = SecretsEnum.Secret;
+        delete process.env[variable];
+        const sut = new SecretsServiceAdapter();
+        jest.spyOn(sut, "getRequiredSecret");
+        let error: Error | null = null;
+		
+        try {
+            const result = sut.getRequiredSecret(variable);
+            expect(result).toBeNull();
+        } catch(e) {
+            error = e;
+        }
+
+        expect(error).toBeInstanceOf(Error);
+        expect(sut.getRequiredSecret).toHaveBeenCalled();
+        expect(sut.getRequiredSecret).toHaveBeenCalledWith(variable);
+    });
+
+    test("Should get required secret | getRequiredSecret", async () => {
+        const variable = SecretsEnum.Secret;
+        process.env[variable] = "Secret";
+        const sut = new SecretsServiceAdapter();
+        jest.spyOn(sut, "getRequiredSecret");
+
+        const result = sut.getRequiredSecret(variable);
+
+        expect(result).toBe("Secret");
+        expect(sut.getRequiredSecret).toHaveBeenCalled();
+        expect(sut.getRequiredSecret).toHaveBeenCalledWith(variable);
+    });
+});
