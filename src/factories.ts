ba549cc5142dfcdae80a17ec11e72078d546bced
João Pedro <112661258+JPedro109@users.noreply.github.com>
2023-12-01 21:09:15 -0300

    refactor: modify factories

--- src/factories.ts
+++ src/factories.ts
@@ -2,9 +2,14 @@ import { QueueServiceAdapter, MailServiceAdapter, SecretsServiceAdapter, QueueHe
 import { SendEmailService } from "@/service";
 import { SendEmailServiceListener } from "@/presentation";
 
+// Infra
 export const secretsServicesAdapter = new SecretsServiceAdapter();
 export const mailServiceAdapter = new MailServiceAdapter(secretsServicesAdapter);
 export const queueHelper = new QueueHelper(secretsServicesAdapter);
 export const queueServiceAdapter = new QueueServiceAdapter(queueHelper);
+
+// Service
 export const sendEmailService = new SendEmailService(mailServiceAdapter);
+
+// Presentation
 export const sendEmailServiceListener = new SendEmailServiceListener(sendEmailService, queueServiceAdapter, secretsServicesAdapter);
