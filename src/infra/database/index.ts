6f9aa82fee751bf68e8da646f9adbf3eee21b5c7
João Pedro <112661258+JPedro109@users.noreply.github.com>
2023-12-12 19:13:19 -0300

    refactor: rename folder

--- src/infra/database/index.ts
+++ src/infra/database/index.ts
@@ -1,6 +1,6 @@
-export * from "./repositories/email-sent/email-sent-model";
-export * from "./repositories/email-sent/email-sent-repository-protocol";
-export * from "./repositories/email-sent/email-sent-repository";
+export * from "./repositories/email-sent-repository/email-sent-model";
+export * from "./repositories/email-sent-repository/email-sent-repository-protocol";
+export * from "./repositories/email-sent-repository/email-sent-repository";
 
 export * from "./repositories/log-repository/log-repository-model";
 export * from "./repositories/log-repository/log-repository-protocol";
