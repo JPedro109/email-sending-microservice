72b65b3a5e7de2073434c294bfdca512e3596933
João Pedro <112661258+JPedro109@users.noreply.github.com>
2024-01-02 20:20:08 -0300

    refactor: add way to set nosql database

--- src/infra/database/repositories/email-sent-repository/email-sent-repository.ts
+++ src/infra/database/repositories/email-sent-repository/email-sent-repository.ts
@@ -3,7 +3,9 @@ import { EmailSentRepositoryProtocol, EmailSentModel, DatabaseNoSQLHelper } from
 import { WithId, Document } from "mongodb";
 
 export class EmailSentRepositoryAdapter implements EmailSentRepositoryProtocol {
-    private readonly collection = "email-sent";
+    private readonly collectionName = "email-sent";
+    private readonly databaseName: string = "email-sending-microservice";
+    private readonly collection = this.databaseNoSQLHelper.getCollection(this.collectionName, this.databaseName);
 
     constructor(private readonly databaseNoSQLHelper: DatabaseNoSQLHelper) { }
 
@@ -19,17 +21,21 @@ export class EmailSentRepositoryAdapter implements EmailSentRepositoryProtocol {
     }
 
     async createEmailSent(email: string, subject: string, template: string, context: string, service: string): Promise<EmailSentModel> {
-        const emailSentCollection = await this.databaseNoSQLHelper.getCollection(this.collection).insertOne({
-            email,
-            subject,
-            template,
-            context,
-            service,
-            created_at: new Date()
-        });
+        const emailSentCollection = await this
+            .databaseNoSQLHelper
+            .getCollection(this.collectionName, this.databaseName)
+            .insertOne({
+                email,
+                subject,
+                template,
+                context,
+                service,
+                created_at: new Date()
+            });
 
-        const emailSentInserted = await this.databaseNoSQLHelper
-            .getCollection(this.collection)
+        const emailSentInserted = await this
+            .databaseNoSQLHelper
+            .getCollection(this.collectionName, this.databaseName)
             .findOne({ _id: emailSentCollection.insertedId });
 
         return this.toMapperEmailSentModel(emailSentInserted);
